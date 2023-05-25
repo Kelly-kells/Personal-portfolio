37a0e5e1bd5073c196dec7f182b13b71107ca5c5
unknown <kellskelechi@gmail.com>
2023-05-25 15:49:18 +0100

    nav-responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { GiHamburgerMenu } from "react-icons/gi"
+import {AiOutlineHome} from "react-icons/ai"
 
 const Navbar = () => {
 
@@ -11,16 +12,44 @@ const Navbar = () => {
     }
     return (
         <div >
-            <GiHamburgerMenu onClick={handleNav} className='absolute top-4 right-4 z-99 md:hidden'/>
+            <GiHamburgerMenu className='absolute top-4 right-4 z-99 md:hidden' />
             {
                 nav ? (
-                   <div>
-                    <a href="#home"></a>
-                   </div>
+                    <div className='fixed w-full h-screen bg-white/90 flex flex-col justify center items-center z-20 '>
+                        <a href="#home" className='w-[75%] flex justify center items-center 
+                        rounded-full shadow-lg bg-Gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-100 ease-in duration-200'>
+                            <AiOutlineHome size={20}/>
+                            <span className='pl-4'>Home</span>
+                        </a>
+
+                        <a href="#home" className='w-[75%] flex justify center items-center 
+                        rounded-full shadow-lg bg-Gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-100 ease-in duration-200'>
+                            <AiOutlineHome size={20}/>
+                            <span className='pl-4'>Work</span>
+                        </a>
+
+                        <a href="#home" className='w-[75%] flex justify center items-center 
+                        rounded-full shadow-lg bg-Gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-100 ease-in duration-200'>
+                            <AiOutlineHome size={20}/>
+                            <span className='pl-4'>Projects</span>
+                        </a>
+
+                        <a href="#home" className='w-[75%] flex justify center items-center 
+                        rounded-full shadow-lg bg-Gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-100 ease-in duration-200'>
+                            <AiOutlineHome size={20}/>
+                            <span className='pl-4'>Resume</span>
+                        </a>
+
+                        <a href="#home" className='w-[75%] flex justify center items-center 
+                        rounded-full shadow-lg bg-Gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-100 ease-in duration-200'>
+                            <AiOutlineHome size={20}/>
+                            <span className='pl-4'>Contact</span>
+                        </a>
+                    </div>
                 )
-                :(
-                    <div></div>
-                )
+                    : (
+                        <div></div>
+                    )
             }
         </div>
     )
