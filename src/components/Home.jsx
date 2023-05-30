0d7ce27b05361a824d4d8a1b869ca09872634e6c
unknown <kellskelechi@gmail.com>
2023-05-30 14:56:44 +0100

    type-animation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./style.css"
 import codeBg from "../assets/codebg.jpg"
+import {TypeAnimation} from "react-type-animation"
 
 
 const Home = () => {
@@ -9,10 +10,29 @@ const Home = () => {
 
       <img className='w-full h-screen object-cover object-left' src={codeBg} alt="" />
       
-  <div class="absolute inset-0 flex items-center justify-center  ">
-    <div class="bg-zinc-100 text-blac w-9/12  h-1/2 shadow-lg  shadow-gray-600 rounded-lg">
-      <div>
-        <h2>FRONTEND</h2>
+  <div class="absolute inset-0 flex items-center justify-center ">
+    <div class="bg-zinc-100 text-blac w-1/2  h-1/2 shadow  shadow-[#16db65] rounded-full  ">
+      <div className='text-center mt-4 '>
+        <h1 className='font-medium text-2xl '>HELLO! I'M FAVOUR KELECHI,</h1>
+        <h2> A<br/>
+
+        <TypeAnimation
+      sequence={[
+        'Frontend developer', // Types 'One'
+        2000, // Waits 1s
+        'Tech enthusiast', // Deletes 'One' and types 'Two'
+        2000, // Waits 2s
+        
+      ]}
+      wrapper="span"
+      cursor={true}
+      repeat={Infinity}
+      style={{ fontSize: '1em', fontWeight: "700" }}
+    />
+
+        </h2>
+        <h4 className='mt-5 font-lg'>Favour is a renowned developer, <br/>with years of experince in developing and <br/>implementing interactive web interfaces. </h4>
+
       </div>
     </div>
   </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { GiHamburgerMenu } from "react-icons/gi"
-import { AiOutlineHome, AiOutlineProject, AiOutlineMail, AiOutlineMacCommand } from "react-icons/ai"
+import { AiOutlineHome, AiOutlineProject, AiOutlineMail, AiOutlineClose} from "react-icons/ai"
 import { GrProjects } from "react-icons/gr"
 import { BsPerson } from "react-icons/bs"
 
@@ -20,31 +20,31 @@ const Navbar = () => {
             {nav ? (
                 <div className='fixed w-full h-screen bg-white/90 flex flex-col justify center items-center z-20 md:hidden'>
                     <a href="#home" className='w-[75%] flex justify center items-center 
-                        rounded-full shadow-lg bg-Gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                        rounded-full shadow-lg bg-Gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-105 ease-in duration-200'>
                         <AiOutlineHome size={20} />
                         <span className='pl-4'>Home</span>
                     </a>
 
                     <a href="#work" className='w-[75%] flex justify center items-center 
-                        rounded-full shadow-lg bg-Gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                        rounded-full shadow-lg bg-Gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-105 ease-in duration-200'>
                         <GrProjects size={20} />
                         <span className='pl-4'>Work</span>
                     </a>
 
                     <a href="#project" className='w-[75%] flex justify center items-center 
-                        rounded-full shadow-lg bg-Gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                        rounded-full shadow-lg bg-Gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-105 ease-in duration-200'>
                         <AiOutlineProject size={20} />
                         <span className='pl-4'>Projects</span>
                     </a>
 
                     <a href="#reume" className='w-[75%] flex justify center items-center 
-                        rounded-full shadow-lg bg-Gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                        rounded-full shadow-lg bg-Gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-105 ease-in duration-200'>
                         <BsPerson size={20} />
                         <span className='pl-4'>Resume</span>
                     </a>
 
                     <a href="#contact" className='w-[75%] flex justify center items-center 
-                        rounded-full shadow-lg bg-Gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                        rounded-full shadow-lg bg-Gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-105 ease-in duration-200'>
                         <AiOutlineMail size={20} />
                         <span className='pl-4'>Contact</span>
                     </a>
@@ -60,7 +60,7 @@ const Navbar = () => {
                 <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
 
 
-                    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Kelly</span>
+                    <span class="self-center text-[#16db65] text-2xl font-semibold whitespace-nowrap dark:text-white">Kelly</span>
 
 
                     <div class="hidden w-full md:block md:w-auto" id="navbar-default">
@@ -78,7 +78,7 @@ const Navbar = () => {
                                 <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:scale-110 ease-in duration-200 md:hover:bg-transparent md:border-0  md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Experience</a>
                             </li>
                             <li>
-                                <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:scale-110 ease-in duration-200 md:hover:bg-transparent md:border-0  md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+                                <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:scale-105 ease-in duration-200 md:hover:bg-transparent md:border-0  md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
                             </li>
                         </ul>
                     </div>
