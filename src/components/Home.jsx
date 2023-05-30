0b5f875f69ec1d251231c26f8d3276aa9bbd70b4
unknown <kellskelechi@gmail.com>
2023-05-30 01:21:31 +0100

    header

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,12 +6,18 @@ import codeBg from "../assets/codebg.jpg"
 const Home = () => {
   return (
     <div className='homeContainer' id='home'>
-<div>
-<img className='w-full h-screen object-cover object-left' src={codeBg} alt="" />
-</div>
 
+      <img className='w-full h-screen object-cover object-left' src={codeBg} alt="" />
+      
+  <div class="absolute inset-0 flex items-center justify-center  ">
+    <div class="bg-zinc-100 text-blac w-9/12  h-1/2 shadow-lg  shadow-gray-600 rounded-lg">
+      <div>
+        <h2>FRONTEND</h2>
+      </div>
+    </div>
+  </div>
 
-        </div>
+    </div>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -55,35 +55,35 @@ const Navbar = () => {
                 )
             }
 
-           
-<nav class="bg-gray-200 shadow-lg  shadow-gray-400 border-gray-200 dark:bg-gray-900">
-  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    
-       
-        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Kelly</span>
-    
-    
-    <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-      <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg  md:flex-row md:space-x-8 md:mt-0 md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 ">
-        <li>
-          <a href="#" class=" block py-2 pl-3 pr-4 font-semibold text-gray-900 bg-blue-700 hover:scale-110 ease-in duration-200 rounded md:bg-transparent text-gray-900  md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
-        </li>
-        <li>
-          <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:scale-110 ease-in duration-200 md:hover:bg-transparent md:border-0  md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Work</a>
-        </li>
-        <li>
-          <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:scale-110 ease-in duration-200 md:hover:bg-transparent md:border-0  md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Projects</a>
-        </li>
-        <li>
-          <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:scale-110 ease-in duration-200 md:hover:bg-transparent md:border-0  md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Experience</a>
-        </li>
-        <li>
-          <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:scale-110 ease-in duration-200 md:hover:bg-transparent md:border-0  md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+
+            <nav class="bg-gray-200 shadow-lg  shadow-gray-400 border-gray-200 dark:bg-gray-900">
+                <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+
+
+                    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Kelly</span>
+
+
+                    <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+                        <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg  md:flex-row md:space-x-8 md:mt-0 md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 ">
+                            <li>
+                                <a href="#" class=" block py-2 pl-3 pr-4 font-semibold text-gray-900 bg-blue-700 hover:scale-110 ease-in duration-200 rounded md:bg-transparent text-gray-900  md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
+                            </li>
+                            <li>
+                                <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:scale-110 ease-in duration-200 md:hover:bg-transparent md:border-0  md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Work</a>
+                            </li>
+                            <li>
+                                <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:scale-110 ease-in duration-200 md:hover:bg-transparent md:border-0  md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Projects</a>
+                            </li>
+                            <li>
+                                <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:scale-110 ease-in duration-200 md:hover:bg-transparent md:border-0  md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Experience</a>
+                            </li>
+                            <li>
+                                <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:scale-110 ease-in duration-200 md:hover:bg-transparent md:border-0  md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
 
         </div>
     )
