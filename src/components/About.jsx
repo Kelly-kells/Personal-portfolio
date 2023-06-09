41c010a1e38318d0abaa89cbce04272d6804955b
unknown <kellskelechi@gmail.com>
2023-06-09 22:45:08 +0100

    project-section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import codeBg from "../assets/codeBg.jpg"
-import {RiMedalFill} from "react-icons/ri"
-import {BsPeopleFill, BsFillFolderFill} from "react-icons/bs"
+import { RiMedalFill } from "react-icons/ri"
+import { BsPeopleFill, BsFillFolderFill } from "react-icons/bs"
 
 import "./style.css"
 
@@ -12,34 +12,34 @@ const About = () => {
         <h1 className='text-xl flex items-center justify-center text-2xl font-bold'> ABOUT ME</h1>
       </div>
       <div className='flex flex-row justify-around mt-20'>
-        <div className='aboutImg w-[18rem] h-[22rem]'>
+        <div className='aboutImg w-[18rem] h-[22rem]  hidden md:block'>
           <img className=' absolute w-[18rem] h-[22rem] about-img' src={codeBg} alt="" />
         </div>
         <div className='about-cards '>
-        <div className='grid md:grid-cols-3 gap-4 sm:grid-cols-2 gap-4 '>
-         <div className="card-contents flex flex-col justify-center text-center p-8 gap-2 ">
-          <RiMedalFill style={{ color: '#067bc2', fontSize: '24px', marginLeft: '30px' }}/>
-          <p className='mt-4'>Experience</p>
-          <small className='text-gray-500'>2+ years working</small>
-         </div>
+          <div className='grid md:grid-cols-3 gap-4 sm:grid-cols-2 gap-4 '>
+            <div className="card-contents flex flex-col justify-center text-center p-8 gap-2 ">
+              <RiMedalFill style={{ color: '#067bc2', fontSize: '24px', marginLeft: '30px' }} />
+              <p className='mt-4'>Experience</p>
+              <small className='text-gray-500'>2+ years working</small>
+            </div>
 
-         <div className="card-contents flex flex-col justify-center  text-center p-8 gap-2" >
-          < BsPeopleFill style={{ color: '#067bc2', fontSize: '24px', marginLeft: '30px' }}/>
-          <p className='mt-4'>Clients</p>
-          <small className='text-gray-500'>on-site/remote</small>
-         </div>
+            <div className="card-contents flex flex-col justify-center  text-center p-8 gap-2" >
+              < BsPeopleFill style={{ color: '#067bc2', fontSize: '24px', marginLeft: '30px' }} />
+              <p className='mt-4'>Clients</p>
+              <small className='text-gray-500'>on-site/remote</small>
+            </div>
 
-         <div className="card-contents flex flex-col justify-center text-center  p-8 gap-2">
-          < BsFillFolderFill style={{ color: '#067bc2', fontSize: '24px', marginLeft: '30px' }}/>
-          <p className='mt-4'>Projects</p>
-          <small className='text-gray-500'>various projects</small>
-         </div>
-        </div>
-        <article className='mt-6'> Favour is an experienced frontend developer, Proficient in Javascript,<br/> it's 
-        liberaries and frameworks.<br/> With adequate understanding of cross-browser compatibility<br/> and web performance optimization,
-        <br/> and the ability to translate design mockups into functional user interfaces. </article>
+            <div className="card-contents flex flex-col justify-center text-center  p-8 gap-2">
+              < BsFillFolderFill style={{ color: '#067bc2', fontSize: '24px', marginLeft: '30px' }} />
+              <p className='mt-4'>Projects</p>
+              <small className='text-gray-500'>various projects</small>
+            </div>
+          </div>
+          <article className='mt-6'> Favour is an experienced frontend developer, Proficient in Javascript,<br /> it's
+            liberaries and frameworks.<br /> With adequate understanding of cross-browser compatibility<br /> and web performance optimization,
+            <br /> and the ability to translate design mockups into functional user interfaces. </article>
 
-        <button className='mt-8 text-center bg-blue-600 text-gray-950 w-40 rounded-lg h-10'><a href="contact">Lets Talk</a></button>
+          <button className='mt-8 text-center bg-blue-600 text-gray-950 w-40 rounded-lg h-10'><a href="contact">Lets Talk</a></button>
         </div>
       </div>
 
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,54 +6,53 @@ import pot from "../assets/africanpot.jpeg"
 
 const Projects = () => {
   return (
-    <section className='w-4/5  h-screen m-auto ' id='projects'>
-
-      
-      <div className="project-container justify-center items-center ">
+    <section className='w-full  h-screen ' id='projects'>
+      <div className="project-container justify-center place-items-center  ">
         <h1 className='text-xl flex items-center justify-center text-2xl font-bold'> MY RECENT PROJECTS</h1>
-        <div className="project-items grid md:grid-cols-4 gap-4 sm:grid-cols-2 gap-4 mt-10 ">
-            <div className='project '>
-         <div className="project-img">
-            <img className='w-44' src={fitness} alt="" />
-         </div>
-         <div className="buttons">
-          <button> <a href="https://github.com/Kelly-kells/fit-fam"></a></button>
-         </div>
+        <div className="flex flex-row justify-center p-10  gap-8 mt-10 ">
+          <div className='project border-2 border-blue-600 rounded-md place-items-center '>
+            <div className="project-img mt-auto ">
+              <img className='w-44 ml-10 mt-8' src={fitness} alt="" />
             </div>
-            
-            <div className='project '>
-         <div className="project-img">
-            <img className='w-40 ' src={filmore} alt="" />
-         </div>
-         <div className="buttons">
-          <button> <a href="https://github.com/Kelly-kells/movies"></a></button>
-         </div>
+            <div className="buttons">
+              <button className='mt-8 text-center bg-blue-600 text-gray-950 w-20 rounded-lg h-10'> <a href="https://github.com/Kelly-kells/fit-fam" target='_blank'></a>Github</button>
             </div>
+          </div>
 
-            <div className='project '>
-         <div className="project-img">
-            <img className='w-40 h-1/3' src={ai} alt="" />
-         </div>
-         <div className="buttons">
-          <button> <a href="https://github.com/Kelly-kells/Ai-project"></a></button>
-         </div>
+          <div className='project border-2 border-blue-600 rounded-md '>
+            <div className="project-img mt-auto ">
+              <img className='w-40 ml-10 mt-8' src={filmore} alt="" />
             </div>
+            <div className="buttons">
+              <button className='mt-8 text-center bg-blue-600 text-gray-950 w-20 rounded-lg h-10'> <a href="https://github.com/Kelly-kells/movies"> Github</a></button>
+            </div>
+          </div>
 
-            <div className='project '>
-         <div className="project-img">
-            <img className='w-40  h-1/2' src={pot} alt="" />
-         </div>
-         <div className="buttons">
-          <button> <a href="https://github.com/Kelly-kells/African-spot"></a></button>
-         </div>
+          <div className='project border-2 border-blue-600 rounded-md'>
+            <div className="project-img mt-auto ">
+              <img className='w-40 h-1/3 ml-10 mt-8' src={ai} alt="" />
             </div>
+            <div className="buttons">
+              <button className='mt-8 text-center bg-blue-600 text-gray-950 w-20 rounded-lg h-10'> <a href="https://github.com/Kelly-kells/Ai-project">Github</a></button>
+            </div>
+          </div>
+
+          <div className='project border-2 border-blue-600 rounded-md'>
+            <div className="project-img mt-auto   ">
+              <img className='w-40  h-1/2 ml-10 mt-8' src={pot} alt="" />
+            </div>
+            <div className="buttons text-center p-4 ">
+              <button className='mt-8 text-center bg-blue-600 text-gray-950 w-20 rounded-lg h-10'> <a href="https://github.com/Kelly-kells/African-spot">Github</a></button>
+              <button className='mt-8 text-center border-2 border-blue-600  w-20 rounded-lg h-10'> <a href="https://github.com/Kelly-kells/African-spot">Github</a></button>
+            </div>
+          </div>
 
 
         </div>
 
       </div>
-    
-    
+
+
     </section >
   )
 }
